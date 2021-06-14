bc8cf9f161245eeba08f2bc0952e2f31a1c236f8
Jesper Ericsson <jjeessppeer@gmail.com>
2021-06-14 09:36:18 +0200

    added gun positions to data

--- public/data.js
+++ public/data.js
@@ -2,72 +2,86 @@ var ships = {
   '0': {
     name: "Unknown",
     img: "images/ships/Unknown.jpg",
-    guns: []
+    guns: [],
+    gun_positions: []
   },
   '1': {
     name: "Goldfish",
     img: "images/ships/Goldfish.jpg",
-    guns: ["HEAVY", "LIGHT", "LIGHT", "LIGHT"]
+    guns: ["HEAVY", "LIGHT", "LIGHT", "LIGHT"],
+    gun_positions: [[133, 425], [212, 552], [52, 557], [98, 728]]
   },
   '2': {
     name: "Junker",
     img: "images/ships/Junker.jpg",
-    guns: ["LIGHT", "LIGHT", "LIGHT", "LIGHT", "LIGHT"]
+    guns: ["LIGHT", "LIGHT", "LIGHT", "LIGHT", "LIGHT"],
+    gun_positions: [[91, 105], [128, 302], [51, 302], [154, 374], [28, 374]]
   },
   '3': {
     name: "Squid",
     img: "images/ships/Squid.jpg",
-    guns: ["LIGHT", "LIGHT", "LIGHT"]
+    guns: ["LIGHT", "LIGHT", "LIGHT"],
+    gun_positions: [[105, 148], [198, 312], [106, 424]]
   },
   '4': {
     name: "Galleon",
     img: "images/ships/Galleon.jpg",
-    guns: ["HEAVY", "HEAVY", "HEAVY", "HEAVY", "LIGHT", "LIGHT"]
+    guns: ["HEAVY", "HEAVY", "HEAVY", "HEAVY", "LIGHT", "LIGHT"],
+    gun_positions: [[172, 276], [70, 276], [192, 380], [51, 380], [34, 405], [120, 590]]
   },
   '5': {
     name: "Spire",
     img: "images/ships/Spire.jpg",
-    guns: ["HEAVY", "LIGHT", "LIGHT", "LIGHT"]
+    guns: ["HEAVY", "LIGHT", "LIGHT", "LIGHT"],
+    gun_positions: [[198, 247], [145, 268], [107, 285], [279, 315]]
   },
   '6': {
     name: "Pyramidion",
     img: "images/ships/Pyramidion.jpg",
-    guns: ["LIGHT", "LIGHT", "LIGHT", "LIGHT"]
+    guns: ["LIGHT", "LIGHT", "LIGHT", "LIGHT"],
+    gun_positions: [[173, 275], [54, 275], [45, 426], [62, 521]]
   },
   '7': {
     name: "Mobula",
     img: "images/ships/Mobula.jpg",
-    guns: ["LIGHT", "LIGHT", "LIGHT", "LIGHT", "LIGHT"]
+    guns: ["LIGHT", "LIGHT", "LIGHT", "LIGHT", "LIGHT"],
+    gun_positions: [[415, 100], [135, 100], [275, 105], [472, 184], [79, 184]]
   },
   '8': {
     name: 'Magnate',
     img: "images/ships/Magnate.jpg",
-    guns: ["HEAVY", "HEAVY", "LIGHT", "LIGHT", "LIGHT", "LIGHT"]
+    guns: ["HEAVY", "HEAVY", "LIGHT", "LIGHT", "LIGHT", "LIGHT"],
+    gun_positions: [[349, 505], [186, 505], [305, 357], [231, 356], [326, 455], [209, 455]]
   },
   '9': {
     name: 'Crusader',
     img: "images/ships/Crusader.jpg",
-    guns: ["HEAVY", "HEAVY", "LIGHT", "LIGHT", "LIGHT", "LIGHT"]
+    guns: ["HEAVY", "HEAVY", "LIGHT", "LIGHT", "LIGHT", "LIGHT"],
+    gun_positions: [[193, 150], [44, 150], [184, 240], [53, 240], [205, 323], [34, 323]]
   },
   '10': {
     name: 'Judgement',
     img: "images/ships/Judgement.jpg",
-    guns: ["HEAVY", "HEAVY", "LIGHT", "LIGHT", "LIGHT"]
+    guns: ["HEAVY", "HEAVY", "LIGHT", "LIGHT", "LIGHT"],
+    gun_positions: [[158, 267], [158, 529], [228, 215], [90, 215], [158, 466]]
   },
   '11': {
     name: 'Corsair',
     img: "images/ships/Corsair.jpg",
-    guns: ["HEAVY", "HEAVY", "HEAVY", "LIGHT", "LIGHT", "LIGHT"]
+    guns: ["HEAVY", "HEAVY", "HEAVY", "LIGHT", "LIGHT", "LIGHT"],
+    gun_positions: [[169, 157], [266, 310], [71, 310], [263, 244], [75, 244], [169, 441]]
   },
   '12': {
     name: 'Shrike',
     img: "images/ships/Shrike.jpg",
-    guns: ["HEAVY", "HEAVY", "LIGHT", "LIGHT"]
+    guns: ["HEAVY", "HEAVY", "LIGHT", "LIGHT"],
+    gun_positions: [[185, 320], [90, 320], [155, 240], [115, 240]]
   },
   '13': {
     name: 'Stormbreaker',
     img: "images/ships/Stormbreaker.jpg",
-    guns: ["HEAVY", "LIGHT", "LIGHT", "LIGHT"]
+    guns: ["HEAVY", "LIGHT", "LIGHT", "LIGHT"],
+    gun_positions: [[200, 212], [90, 150], [80, 255], [50, 300]]
   }
 };
 
